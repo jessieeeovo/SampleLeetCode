095630a6927cc6fb629a23d574ca227005283673
Jingqi Yao <56903646+jessieeeovo@users.noreply.github.com>
2023-03-19 10:53:40 -0700

    Time: 856 ms (100.00%), Space: 28.7 MB (100.00%) - LeetHub

---
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        # Weekly Contest 337, 03/19/2023
+        count = [0] * value
+        for n in nums:
+            count[n % value] += 1
+        m = min(count)
+        res = m * value
+        for c in count:
+            if c > m:
+                res += 1
+            else:
+                break
+        return res
+                
